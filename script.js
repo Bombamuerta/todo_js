b31b16ee303b695c50b00944361c721bfaa8b640
Bombamuerta <dsnakez11@yandex.ru>
2020-04-29 16:32:21 +0300

    local storage added + fixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addBtn = document.querySelector('.form-input-button')
 const todoList = document.querySelector('.todo-list')
 const filterOptions = document.querySelector('.filter-todo')
 
-const addItem = e => {
+const addItem = (e) => {
   e.preventDefault()
   if (formInput.value !== '') {
     const newItem = document.createElement('li')
@@ -15,8 +15,6 @@ const addItem = e => {
     newTodoText.classList.add('todo-text')
     saveLocalTodos(formInput.value)
     newItem.appendChild(newTodoText)
-     
-    formInput.value = ''
 
     completeBtn = document.createElement('button')
     completeBtn.classList.add('todo-complete')
@@ -27,20 +25,23 @@ const addItem = e => {
     removeBtn.classList.add('todo-remove')
     removeBtn.innerText = 'Delete'
     newItem.appendChild(removeBtn)
+
+    formInput.value = ''
   } else {
     alert('write something!')
   }
 }
 
-const deleteCheck = e => {
+const deleteCheck = (e) => {
   const item = e.target
   if (item.classList[0] === 'todo-remove') {
     const todo = item.parentElement
+    removeLocalTodos(todo)
     todo.remove()
   }
 }
 
-const completeToggle = e => {
+const completeToggle = (e) => {
   const item = e.target
   if (item.classList[0] === 'todo-complete') {
     const todo = item.parentElement
@@ -48,10 +49,9 @@ const completeToggle = e => {
   }
 }
 
-const filterTodo = e => {
+const filterTodo = (e) => {
   todos = todoList.childNodes
-  console.log(todos)
-  todos.forEach(todoItem => {
+  todos.forEach((todoItem) => {
     switch (e.target.value) {
       case 'all':
         todoItem.style.display = 'flex'
@@ -69,39 +69,67 @@ const filterTodo = e => {
         } else {
           todoItem.style.display = 'none'
         }
-        break
     }
   })
 }
 
-
 const saveLocalTodos = (localTodo) => {
-  let todos
-  if(localStorage.getItem(todos) === null){
-    todos = []
+  let todosArr
+  if (localStorage.getItem('todosArr') === null) {
+    todosArr = []
   } else {
-    todos = JSON.parse(localStorage.getItem('todos'))
+    todosArr = JSON.parse(localStorage.getItem('todosArr'))
   }
-  todos.push(localTodo)
-  localStorage.setItem('todos', JSON.stringify(todos))
+  todosArr.push(localTodo)
+  localStorage.setItem('todosArr', JSON.stringify(todosArr))
 }
 
+const getLocalTodos = () => {
+  let todosArr
+  if (localStorage.getItem('todosArr') === null) {
+    todosArr = []
+  } else {
+    todosArr = JSON.parse(localStorage.getItem('todosArr'))
+  }
+  todosArr.forEach((itemTodo) => {
+    const newItem = document.createElement('li')
+    newItem.classList.add('todo-list-item')
 
+    todoList.appendChild(newItem)
+    newTodoText = document.createElement('span')
+    newTodoText.innerText = itemTodo
+    newTodoText.classList.add('todo-text')
 
+    newItem.appendChild(newTodoText)
+    completeBtn = document.createElement('button')
+    completeBtn.classList.add('todo-complete')
+    completeBtn.innerText = 'Done!'
 
-// local store test
-// const arr = ['kill', 'crush', 'destroy']
+    newItem.appendChild(completeBtn)
+    removeBtn = document.createElement('button')
+    removeBtn.classList.add('todo-remove')
+    removeBtn.innerText = 'Delete'
 
-// localStorage.setItem(arr, JSON.stringify(arr))
-// console.log(arr)
+    newItem.appendChild(removeBtn)
+  })
+}
 
-// const getLocal = JSON.parse(localStorage.getItem(arr))
-// console.log(arr)
+const removeLocalTodos = (todo) => {
+  let todosArr
+  if (localStorage.getItem('todosArr') === null) {
+    todosArr = []
+  } else {
+    todosArr = JSON.parse(localStorage.getItem('todosArr'))
+  }
+  todoIndex = todo.children[0].innerText
+  todosArr.splice(todosArr.indexOf(todoIndex), 1)
 
+  localStorage.setItem('todosArr', JSON.stringify(todosArr))
+  console.log(todo.children[0].innerText)
+}
 
-
+document.addEventListener('DOMContentLoaded', getLocalTodos)
 addBtn.addEventListener('click', addItem)
 todoList.addEventListener('click', deleteCheck)
 todoList.addEventListener('click', completeToggle)
 filterOptions.addEventListener('click', filterTodo)
-
